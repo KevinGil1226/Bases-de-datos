7521fa1b391d310e38cc3d42a88bf547107e3c71
Kevin Ballen <kballengiraldo@gmail.com>
2024-08-24 11:51:49 -0500

    basedatos

--- empleados.sql
+++ empleados.sql
@@ -7,8 +7,14 @@ CREATE TABLE tbl_empleados(
     nombres VARCHAR(20) NOT NULL,
     apellidos VARCHAR(20) NOT NULL,
     direccion VARCHAR (25) NOT NULL,
-    departamento VARCHAR (20) NOT NULL
-    ciudad VARCHAR (20) NOT NULL
-
-
+    departamento VARCHAR (20) NOT NULL,
+    ciudad VARCHAR (20) NOT NULL,
+    género VARCHAR (15) NOT NULL,
+    correopersonal VARCHAR (30) NOT NULL,
+    correocorporativo VARCHAR (30) NOT NULL,
+    cargo VARCHAR (20) NOT NULL,
+    area VARCHAR (20) NOT NULL,
+    telefono INT NOT NULL,
+    telefono_opcional INT NULL,
+    PRIMARY KEY (idIdentificacion)
 )
